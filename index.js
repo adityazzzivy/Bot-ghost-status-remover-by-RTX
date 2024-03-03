8f76ec52099486c6903653ffd5de85d81904c73f
adityazzzivy <162062494+adityazzzivy@users.noreply.github.com>
2024-03-03 14:07:47 -0800

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["BRAWL STARS👑 ","BRAWL STARS👑 "];
 
 
 let currentIndex = 0;
